39c6a1bc64017539bdb8a0c329ee1541ca5fd43f
Mohit Gusain <mohitgusain8671@gmail.com>
2025-07-20 16:16:30 +0530

    Creating Home Page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMate" },
+    { name: "description", content: "AI-powered tool that analyzes resumes, scores them for job fit, and provides smart insights!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-18">
+        <h1>
+          Track Your Applications & Resume Ratings
+        </h1>
+        <h2>
+          AI-powered tool that analyzes resumes, scores them for job fit, and provides smart insights!
+        </h2>
+      </div>
+      {
+        resumes.length > 0 && <div className="resumes-section">
+          {resumes.map((resume: Resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      }
+    </section>
+    
+  </main>;
 }
